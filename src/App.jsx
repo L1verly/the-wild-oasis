523ffe97c65f2ff532691fa808acc771b61b7e31
L1verly <artemartem7975@gmail.com>
2026-02-21 12:56:17 +0100

    fixing missing QueryClientProvider import

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
