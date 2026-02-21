de0f8f8dddc0b8a08bc1ac8d67b76d931510ab92
L1verly <artemartem7975@gmail.com>
2026-02-21 12:52:35 +0100

    setting up supabase and react-query

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,20 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,6 +39,7 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
---
+++ src/services/apiCabins.js
@@ -0,0 +1,10 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select();
+  if (error) {
+    console.error(error);
+    throw new Error("There was a problem loading cabins");
+  }
+  return data;
+}
