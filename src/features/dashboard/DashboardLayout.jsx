b47204599fedb534e4a1b2adcbb3dd76aeb6cc02
L1verly <artemartem7975@gmail.com>
2026-03-09 11:05:40 +0100

    Dashboard scaffolding

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Activity</div>
+      <div>Activity</div>
+      <div>Chart</div>
+      <div>Chart</div>
+    </StyledDashboardLayout>
+  );
+}
