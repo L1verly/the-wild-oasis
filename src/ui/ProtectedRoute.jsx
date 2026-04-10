e7991dcd3a237b8a94dc0a655f27c419951769d4
L1verly <artemartem7975@gmail.com>
2026-04-10 14:42:16 +0200

    Cleared unused components

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,16 +1,7 @@
-import styled from "styled-components";
 import { useUser } from "../features/authentication/useUser";
 import { useNavigate } from "react-router";
 import { FullPageSpinner } from "./Spinner";
 import { useEffect } from "react";
-
-const FullPage = styled.div`
-  height: 100vh;
-  background-color: var(--color-grey-50);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 
 export default function ProtectedRoute({ children }) {
   const navigate = useNavigate();
